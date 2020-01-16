dd808a09857ec4bf844a9ce97c672881d0b5c9a5
Matt Owen <consistencyhobgoblin-github@yahoo.com>
2020-01-16 11:12:36 -0800

    remove null from model strings

--- ad-weigh/app/models.py
+++ ad-weigh/app/models.py
@@ -7,7 +7,7 @@ from django.db.models import Sum
 class metric_grouping(models.Model):
     """an object to store 'dashboard', 'report' or other grouping of metrics"""
     grouping_name = models.CharField(max_length=200)
-    grouping_description = models.CharField(max_length=1200, null=True, blank=True)
+    grouping_description = models.CharField(max_length=1200, blank=True)
     enum_grouping_type = models.CharField(max_length=50)
 
     def __unicode__(self):
@@ -20,7 +20,7 @@ class metric_grouping(models.Model):
 class metric_category(models.Model):
     """an object to store 'CRM', 'Display', 'Paid Search' et al."""
     category_name = models.CharField('category', max_length=50)
-    normalized_table_name = models.CharField('datamart table', max_length=100, null=True, blank=True)
+    normalized_table_name = models.CharField('datamart table', max_length=100, blank=True)
 
     def __unicode__(self):
         """Returns a string representation of a poll."""
@@ -34,7 +34,7 @@ class metric_type(models.Model):
     type_metric_category = models.ForeignKey(metric_category)
     type_metric_grouping = models.ManyToManyField(metric_grouping, blank=True)
     type_name = models.CharField('metric type', max_length=50)
-    enum_datatype = models.CharField('datatype', max_length=50, null=True, blank=True)
+    enum_datatype = models.CharField('datatype', max_length=50, blank=True)
 
     def __unicode__(self):
         """Returns a string representation of a poll."""
@@ -49,8 +49,8 @@ class metric_value(models.Model):
     when_metric_begin = models.DateTimeField('metric begin date', null=True, blank=True)
     when_metric_end = models.DateTimeField('metric end date', null=True, blank=True)
     numeric_value = models.FloatField('number value', default=0.0)
-    string_value = models.CharField('text value', max_length=1000, null=True, blank=True)
+    string_value = models.CharField('text value', max_length=1000, blank=True)
     boolean_value = models.NullBooleanField('true-false value')
     is_rate = models.BooleanField(default=False)
-    notes = models.TextField('notes', null=True, blank=True)
+    notes = models.TextField('notes', blank=True)
   
