03248da8c7c1970d95e36e1d6812791cf51748d1
Matt Owen <consistencyhobgoblin-github@yahoo.com>
2020-02-19 16:50:59 -0800

    added table formatting for value entry.  abandoned datapicker attempts.

--- ad-weigh/app/forms.py
+++ ad-weigh/app/forms.py
@@ -19,9 +19,21 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
 
+#class ValuesEntryForm(forms.ModelForm):
+#    model = metric_value
+#    when_metric_begin=forms.DateField(widget=forms.SelectDateWidget)
+#    when_metric_end=forms.DateField(widget=forms.SelectDateWidget)
+#    numeric_value=forms.CharField(widget=forms.NumberInput)
+#    notes=forms.CharField(widget=forms.TextInput(attrs={'size':'40'}))
+
+
 class ValuesEntryForm(forms.ModelForm):
+    lstYears = ['1900', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
     model = metric_value
-    when_metric_begin=forms.DateField(widget=forms.SelectDateWidget)
-    when_metric_end=forms.DateField(widget=forms.SelectDateWidget)
-    numeric_value=forms.CharField(widget=forms.NumberInput)
-    notes=forms.CharField(widget=forms.TextInput(attrs={'size':'40'}))
+    es_attrs = {'class': 'form-control'}
+    nm_attrs = {'class': 'form-control', 'step': 0.01}
+    when_metric_begin=forms.DateField(widget=forms.SelectDateWidget(years=lstYears))
+    when_metric_end=forms.DateField(widget=forms.SelectDateWidget(years=lstYears))
+    #when_metric_end=forms.DateField(widget=forms.SelectDateWidget(years=range(2017, 2040)))
+    numeric_value=forms.CharField(widget=forms.NumberInput(attrs=nm_attrs))
+    notes=forms.CharField(widget=forms.TextInput(attrs=es_attrs))
